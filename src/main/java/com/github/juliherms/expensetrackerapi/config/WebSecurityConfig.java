d7072fe65a9eb688979dbe0ea592d698d387e6c4
Juliherms De Lima Vasconcelos <juliherms.vasconcelos@exmed.com.br>
2024-06-18 11:05:48 -0300

    chore: update spring boot

--- src/main/java/com/github/juliherms/expensetrackerapi/config/WebSecurityConfig.java
+++ src/main/java/com/github/juliherms/expensetrackerapi/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,17 +27,13 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers(LOGIN_PATH, REGISTER_PATH).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-        http.httpBasic();
-        return http.build();
+        return http.csrf(csrf -> csrf.disable())
+                        .authorizeHttpRequests(auth -> auth.requestMatchers(LOGIN_PATH,REGISTER_PATH).permitAll().anyRequest().authenticated())
+                        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
+                        .httpBasic(Customizer.withDefaults())
+                        .build();
     }
 
     @Bean
